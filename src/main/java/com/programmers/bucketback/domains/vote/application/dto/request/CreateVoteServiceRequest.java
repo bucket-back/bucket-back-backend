0af48c9e780d3d4853cb038feca98e75b4a33aed
Yiseul Park <omjl5123@catholic.ac.kr>
2023-10-29 22:59:08 +0900

    refactor: 서비스 dto에서 @NotNull 제거

--- src/main/java/com/programmers/bucketback/domains/vote/application/dto/request/CreateVoteServiceRequest.java
+++ src/main/java/com/programmers/bucketback/domains/vote/application/dto/request/CreateVoteServiceRequest.java
@@ -2,21 +2,13 @@ package com.programmers.bucketback.domains.vote.application.dto.request;
 
 import com.programmers.bucketback.domains.common.Hobby;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record CreateVoteServiceRequest(
-	@NotNull
 	Hobby hobby,
-
-	@NotNull
 	String content,
-
-	@NotNull
 	Long option1ItemId,
-
-	@NotNull
 	Long option2ItemId
 ) {
 }
