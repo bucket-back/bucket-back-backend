a4e27d8fad47304411ee5413f2506e39530ad54a
Curry4182 <kangbk4182@naver.com>
2023-11-28 18:37:33 +0900

    style: 피드 목록 조회 변수명을 요구사항 의미를 반영하여 변경

--- bucketback-domain/src/main/java/com/programmers/bucketback/domains/feed/repository/FeedRepositoryForCursorImpl.java
+++ bucketback-domain/src/main/java/com/programmers/bucketback/domains/feed/repository/FeedRepositoryForCursorImpl.java
@@ -33,8 +33,8 @@ public class FeedRepositoryForCursorImpl implements FeedRepositoryForCursor {
 	private final JPAQueryFactory jpaQueryFactory;
 
 	public List<FeedCursorSummary> findAllByCursor(
-		final Long myPageMemberId,
-		final boolean myPageOwnerLikeFeeds,
+		final Long nicknameMemberId,
+		final boolean onlyNicknameLikeFeeds,
 		final Hobby hobby,
 		final FeedSortCondition feedSortCondition,
 		final String cursorId,
@@ -44,12 +44,12 @@ public class FeedRepositoryForCursorImpl implements FeedRepositoryForCursor {
 			.from(feed)
 			.where(
 				feed.bucketInfo.hobby.eq(hobby),
-				eqMemberId(myPageMemberId),
+				eqMemberId(nicknameMemberId, onlyNicknameLikeFeeds),
 				lessThanNextCursorId(feedSortCondition, cursorId)
 			).limit(pageSize)
 			.fetch();
 
-		if (myPageOwnerLikeFeeds) {
+		if (onlyNicknameLikeFeeds) {
 			feedIds = jpaQueryFactory.select(feedLike.feed.id)
 				.from(feedLike)
 				.where(
@@ -63,7 +63,7 @@ public class FeedRepositoryForCursorImpl implements FeedRepositoryForCursor {
 			.where(
 				feedItem.feed.id.in(feedIds)
 			)
-			.join(member).on(eqMemberIdToJoin(myPageMemberId))
+			.join(member).on(eqMemberIdToJoin(nicknameMemberId))
 			.orderBy(feedSort(feedSortCondition), feed.id.desc())
 			.transform(
 				groupBy(feed.id)
